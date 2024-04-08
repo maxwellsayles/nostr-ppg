09df747eabddaacf0da535a36124829cf9c9103b
Maxwell Sayles <maxwellsayles@gmail.com>
2024-04-07 17:34:21 -0700

    Remove orphaned code.

--- src/ppg.rs
+++ src/ppg.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
 use std::thread;
-use std::time::Duration;
 
 use nostr_sdk::prelude as nostr;
 use nostr_sdk::prelude::{Result};
@@ -29,55 +28,6 @@ async fn new_keys_route() -> Result<impl warp::Reply, warp::Rejection> {
 	},
 	Err(_) => Err(warp::reject::reject()),
     }
-}
-
-#[allow(dead_code)]
-async fn set_metadata(client: &nostr::Client) -> Result<()> {
-    let metadata = nostr::Metadata::new()
-        .name("fysx-nostr-ppg")
-        .display_name("Fysx Nostr PPG")
-        .about("Personal Playground for Nostr dev.")
-        .lud16("weirdmexican50@walletofsatoshi.com");
-    client.set_metadata(&metadata).await?;
-    Ok(())
-}
-
-#[allow(dead_code)]
-async fn get_events() -> Result<()> {
-    // Create client from secret key.
-    let keys = nostr::Keys::parse(fs::read_to_string(".nsec")?)?;
-
-    // Show bech32 public key
-    let pubkey = keys.public_key();
-    let bech32_pubkey: String = pubkey.to_bech32()?;
-    println!("Bech32 PubKey: {}", bech32_pubkey);
-
-    // Create new client
-    let client = nostr::Client::new(&keys);
-    client.add_relay("wss://relay.damus.io").await?;
-    client.connect().await;
-
-    // Get metadata.
-    let filter = nostr::Filter::new().author(pubkey).kind(nostr::Kind::Metadata);
-    let events = client
-        .get_events_of(vec![filter], Some(Duration::from_secs(10)))
-        .await?;
-    println!("{events:#?}");
-
-    // Get text notes.
-    let filter = nostr::Filter::new()
-        .author(pubkey)
-        .kind(nostr::Kind::TextNote)
-        .limit(3);
-    let events = client
-        .get_events_of(
-            vec![filter],
-            Some(Duration::from_secs(10)),
-        )
-        .await?;
-    println!("{events:#?}");
-
-    Ok(())
 }
 
 fn load_keys() -> Result<nostr::Keys> {
